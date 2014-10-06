9e1edbeef491f426c460fcd005ecb2e8b629bc63
Kevin Lin <lin.kevin.01@gmail.com>
2014-10-06 19:01:05 -0400

    initial commit

---
+++ HelpScoutDAL/HelpScoutApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpScoutDAL
+{
+    public class HelpScoutApi
+    {
+        private WebClient _client;
+
+        public HelpScoutApi()
+        {
+            _client = new WebClient();
+        }
+    }
+}
