b0cc2a3ed2209782eabc4aae0c4a7c67e8b8affe
Kevin Lin <kevinlin@Kevins-MacBook-Pro.local>
2014-10-06 20:38:25 -0400

    added helpscout api key

--- HelpScoutDAL/HelpScoutApi.cs
+++ HelpScoutDAL/HelpScoutApi.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace HelpScoutDAL
 {
     public class HelpScoutApi
     {
         private WebClient _client;
+        private readonly string _apiKey = ConfigurationManager.AppSettings["HelpScoutApiKey"];
 
         public HelpScoutApi()
         {
-            _client = new WebClient();
+            _client = new WebClient
+            {
+                Credentials = new NetworkCredential(_apiKey, "X")
+            };
         }
+
+        public object GetConversations()
+        {
+            return null;
+        }
+
+
     }
 }
